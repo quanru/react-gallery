bc6886225e37fba591d8e174da445550b98d6e53
醍醐 <tihu@wacai.com>
2018-07-02 10:57:44 +0800

    feat: 添加格式支持

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -53,7 +53,7 @@ function getDefaultModules() {
         loader: 'url-loader?limit=8192'
       },
       {
-        test: /\.(mp4|ogg|svg)$/,
+        test: /\.(mp4|ogg|svg|mov)$/,
         loader: 'file-loader'
       },
       {
